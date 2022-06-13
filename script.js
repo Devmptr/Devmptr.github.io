3c29d372a1ffdbef895051752090999a5cfedf3e
Devmptr <dmahaputra@student.unud.ac.id>
2022-06-13 11:48:53 +0700

    Initial

---
+++ script.js
@@ -0,0 +1,108 @@
+let prevNumber = '';
+let calculationOperator = '';
+let currentNumber = '0';
+
+const inputNumber = (number) => {
+  if (currentNumber === '0') {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
+}
+
+const calculatorScreen = document.querySelector(".calculator-screen");
+
+const updateScreen = (number) => {
+  if (prevNumber !== '' && calculationOperator !== '' && currentNumber !== '') {
+    calculatorScreen.value = `${prevNumber} ${calculationOperator} ${currentNumber}`;
+  } else if (prevNumber !== '' && calculationOperator !== '') {
+    calculatorScreen.value = `${prevNumber} ${calculationOperator}`;
+  } else {
+    calculatorScreen.value = number;
+  }
+}
+
+const numbers = document.querySelectorAll(".number");
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
+  });
+});
+
+const inputOperator = (operator) => {
+  if (calculationOperator === '') {
+    prevNumber = currentNumber;
+  }
+  calculationOperator = operator;
+  currentNumber = '';
+}
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+    updateScreen();
+  });
+});
+
+const equalSign = document.querySelector(".equal-sign");
+
+equalSign.addEventListener("click", () => {
+  calculate();
+  updateScreen(currentNumber);
+});
+
+const calculate = () => {
+  let result = '';
+  switch(calculationOperator) {
+    case "+":
+      result = parseFloat(prevNumber) + parseFloat(currentNumber);
+      break;
+    case "-":
+      result = parseFloat(prevNumber) - parseFloat(currentNumber);
+      break;
+    case "*":
+      result = parseFloat(prevNumber) * parseFloat(currentNumber);
+      break;
+    case "/":
+      result = parseFloat(prevNumber) / parseFloat(currentNumber);
+      break;
+    default:
+      break;
+  }
+
+  currentNumber = result;
+  calculationOperator = '';
+}
+
+const clearAll = () => {
+  prevNumber = "";
+  calculationOperator = "";
+  currentNumber = "0";
+}
+
+const clearBtn = document.querySelector('.all-clear');
+
+clearBtn.addEventListener("click", () => {
+  clearAll();
+  updateScreen(currentNumber);
+});
+
+const inputDecimal = (dot) => {
+  if (currentNumber.includes('.')) {
+    return;
+  }
+  currentNumber += dot;
+}
+
+const decimal = document.querySelector('.decimal');
+
+decimal.addEventListener("click", (event) => {
+  inputDecimal(event.target.value);
+  updateScreen(currentNumber);
+});
+
+updateScreen(currentNumber);
